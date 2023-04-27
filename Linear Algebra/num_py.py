ce2d2790940c69103179d256e6ee07a7e3d2413d
Daham-Mustaf <s7damust@uni-bonn.de>
2023-04-27 20:27:36 +0200

    Layer of Neurons

--- Linear Algebra/num_py.py
+++ Linear Algebra/num_py.py
@@ -17,6 +17,42 @@ def simple_neuron(inputs, weights, bias):
     output = max(0, weighted_sum)
     
     return output
+# compute A Layer of Neurons 
+
+def layer(inputs, weights, biases, activation):
+    # Compute the weighted sum of the inputs
+    weighted_sum = np.dot(inputs, weights) + biases
+    
+    # Apply the activation function
+    output = activation(weighted_sum)
+    
+    return output
+
+# Inputs to the layer
+inputs = np.array([1.0, 2.0, 3.0])
+
+# Weights for the layer (3 neurons with 4 inputs each)
+weights = np.array([
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+])
+
+# Biases for the layer (3 neurons)
+biases = np.array([2.0, 3.0, 0.5])
+
+# Activation function for the layer (ReLU)
+def relu(x):
+    return np.maximum(0, x)
+
+# Compute the layer output
+output = layer(inputs, weights, biases, relu)
+
+# Print the output
+print(output) # Expected output: [4.8 1.21 2.39]
+
+
+
 inputs = [1.0, 2.0, 3.0, 2.5]
 weights = [0.2, 0.8, -0.5, 1.0]
 bias = 2.0
